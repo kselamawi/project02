3779e6c6f2e04237af61521423a2eeef0441d20e
isaiah9509 <payne15isaiah@gmail.com>
2022-02-22 14:31:09 -0500

    Changed base strcuture? using Spring

---
+++ MTSBoot/src/main/java/com/example/MtsBootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsBootApplication.class, args);
+	}
+
+}

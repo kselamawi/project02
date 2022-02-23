5e6b31896bea5b6b7ba8cf9190c0cd05c2cea15a
isaiah9509 <payne15isaiah@gmail.com>
2022-02-23 05:55:16 -0500

    Working on user page

--- movieshowings/src/app/app.module.ts
+++ movieshowings/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    UserPageComponent,
+    UpdateUserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

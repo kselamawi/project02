4b00223b3604b592f15db231469dcd7532c9a7cd
amsvang <amyvangsinbox@outlook.com>
2022-02-24 11:35:26 -0800

    updated purchases

--- MTSBoot/src/main/java/com/revature/controllers/PurchaseController.java
+++ MTSBoot/src/main/java/com/revature/controllers/PurchaseController.java
@@ -1,22 +1,29 @@
 package com.revature.controllers;
 
 import com.revature.models.Purchase;
+import com.revature.models.User;
 import com.revature.services.PurchaseService;
+import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @Controller
 @RequestMapping(value="/purchase")
 @CrossOrigin("*")
 public class PurchaseController {
+    private UserService us;
     private PurchaseService ps;
 
     public PurchaseController(){}
 
     @Autowired
-    public PurchaseController(PurchaseService ps) {
+    public PurchaseController(PurchaseService ps, UserService us) {
         this.ps = ps;
+        this.us = us;
     }
 
     @PostMapping("/")
@@ -26,13 +33,14 @@ public class PurchaseController {
         return ps.createPurchase(purchase.getPurchaseId(), purchase.getPurchaseDate(), purchase.getOwner());
     }
 
-    @GetMapping("/{purchaseId}")
-    @ResponseBody
-    Purchase getPurchaseById(@PathVariable("purchaseId")int purchaseId) {
-        Purchase purchase = ps.getPurchaseById(purchaseId);
+    // This method gets all purchase by user (waiting on UserService to be created) *********************************
 
-        return purchase;
-    }
+//    @GetMapping("/user/{id}")
+//    @ResponseBody
+//    public List<Purchase> getPurchaseByUserId(@PathVariable("id")int id) {
+//         User user = us.getById(id);
+//         return ps.getPurchasesByUser(user);
+//    }
 
     @DeleteMapping("/{purchaseId}")
     @ResponseBody

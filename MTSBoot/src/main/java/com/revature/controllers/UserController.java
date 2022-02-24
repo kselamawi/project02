e67d5db5ec8b783e9303e6b45d15ed2bddfd36c2
isaiah9509 <payne15isaiah@gmail.com>
2022-02-24 18:41:42 -0500

    Added login feature. (I think)

--- MTSBoot/src/main/java/com/revature/controllers/UserController.java
+++ MTSBoot/src/main/java/com/revature/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.revature.controllers;
 
 
+import com.revature.exceptions.NotAValidLogin;
 import com.revature.models.User;
 import com.revature.repository.UserRepository;
 import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,6 +49,15 @@ public class UserController {
     @ResponseBody
     public User getUserById(@PathVariable("id")int id){
     return us.getUserById(id);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User u, HttpSession session) throws NotAValidLogin {
+        if(us.login(u.getEmail())) {
+            session.setAttribute("id", u.getId());
+            return new ResponseEntity<String>("YES", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Wrong login information", HttpStatus.FORBIDDEN);
     }
 
 }

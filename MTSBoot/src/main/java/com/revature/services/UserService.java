3e5491d6a5c9e2d0468572548d949ca70dfdaa1c
isaiah9509 <payne15isaiah@gmail.com>
2022-02-24 14:42:43 -0500

    Commiting changes

--- MTSBoot/src/main/java/com/revature/services/UserService.java
+++ MTSBoot/src/main/java/com/revature/services/UserService.java
@@ -1,5 +1,10 @@
 package com.revature.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class UserService {
 
 
--- movieshowings/src/app/app.module.ts
+++ movieshowings/src/app/app.module.ts
@@ -1,32 +1,27 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { UserPageComponent } from './components/user-page/user-page.component';
 import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     UserPageComponent,
     UpdateUserComponent,
-=======
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
     RegisterComponent,
     LoginComponent
->>>>>>> ce5cb435937e9248c76a63437952f7ab7a5ecfdd
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

ce5cb435937e9248c76a63437952f7ab7a5ecfdd
NabiKalokoh <nabiatu1@gmail.edu>
2022-02-24 14:18:50 -0500

    able to grab data from user on register, working on user service

--- movieshowings/src/app/app.module.ts
+++ movieshowings/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

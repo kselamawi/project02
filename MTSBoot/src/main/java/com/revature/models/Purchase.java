ee30e70ddb8cf3a60175ad7786a2430587ead552
amsvang <amyvangsinbox@outlook.com>
2022-02-24 08:04:37 -0800

    created purchaseController, Repository, and Service

--- MTSBoot/src/main/java/com/revature/models/Purchase.java
+++ MTSBoot/src/main/java/com/revature/models/Purchase.java
@@ -1,15 +1,67 @@
 package com.revature.models;
 
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.Date;
 
-
+@Entity
+@Table(name="people")
 public class Purchase {
 
-    private int id;
-    private Date date;
+    @Id
+    @Column(name="purchaseId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int purchaseId;
+
+    @Column(name="purchase_date", nullable = false)
+    private Date purchaseDate;
+
+    @Column(name="userId")
     private int userId;
 
-    //Ticket
-    private Ticket ticket;
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+    private List<Ticket> tickets = new ArrayList<>();
 
+
+    public Purchase() {}
+
+    public Purchase(int id, Date purchaseDate, int userId) {
+        this.purchaseId = id;
+        this.purchaseDate = purchaseDate;
+        this.userId = userId;
+    }
+
+    public int getPurchaseId() {
+        return purchaseId;
+    }
+
+    public void setPurchaseId(int purchaseId) {
+        this.purchaseId = purchaseId;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
---
+++ MTSBoot/src/main/java/com/revature/services/PurchaseService.java
@@ -0,0 +1,33 @@
+package com.revature.services;
+
+import com.revature.models.Purchase;
+import com.revature.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+@Transactional
+public class PurchaseService {
+    private PurchaseRepository pr;
+
+    public PurchaseService() {}
+
+    @Autowired
+    public PurchaseService(PurchaseRepository pr) {
+        this.pr = pr;
+    }
+
+    public Purchase createPurchase(int purchaseId, Date purchaseDate, int userId) {
+        Purchase purchase = new Purchase(purchaseId, (java.sql.Date) purchaseDate, userId);
+        return pr.save(purchase);
+    }
+
+    public void deletePurchase(Purchase purchase) {
+       pr.delete(purchase);
+    }
+
+
+}

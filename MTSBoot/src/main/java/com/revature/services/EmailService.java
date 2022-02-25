3a9d9a6213bf274357b0034d06cedf67b490e158
germygrimes <germygrimes@gmail.com>
2022-02-25 15:14:59 -0500

    added maven dependency for javamail. Still trying to get it to work.

---
+++ MTSBoot/src/main/java/com/revature/services/EmailService.java
@@ -0,0 +1,20 @@
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+
+public class EmailService {
+
+    public static void main(String args[]) {
+        try {
+            String smtpServer = args[0];
+            String to = args[1];
+            String from = args[2];
+            String subject = args[3];
+            String body = args[4];
+            send(smtpServer, to, from, subject, body);
+        } catch (Exception e) {
+            System.out.println("Usage: smtpServer toAddress fromAddress subjectText bodyText");
+        }
+        System.exit(0);
+    }
+}

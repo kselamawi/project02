6c8352cbbb30038506276299f2857078236d56a7
isaiah9509 <payne15isaiah@gmail.com>
2022-02-28 14:45:58 -0500

    Adjusted login/logout to use cookies

--- MTSBoot/src/main/java/com/revature/models/Ticket.java
+++ MTSBoot/src/main/java/com/revature/models/Ticket.java
@@ -1,7 +1,6 @@
 package com.revature.models;
 
 import javax.persistence.*;
-import java.security.acl.Owner;
 import java.util.Date;
 
 @Entity
@@ -32,5 +31,64 @@ public class Ticket {
     @JoinColumn(name="purchase")
     public Purchase purchase;
 
+    public Ticket (){
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getShowTimeDate() {
+        return showTimeDate;
+    }
+
+    public void setShowTimeDate(Date showTimeDate) {
+        this.showTimeDate = showTimeDate;
+    }
+
+    public String getMovie_name() {
+        return movie_name;
+    }
+
+    public void setMovie_name(String movie_name) {
+        this.movie_name = movie_name;
+    }
+
+    public Purchase getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(Purchase purchase) {
+        this.purchase = purchase;
+    }
+
+    public Ticket(int id, User owner, double price, Date showTimeDate, String movie_name, Purchase purchase) {
+        this.id = id;
+        this.owner = owner;
+        this.price = price;
+        this.showTimeDate = showTimeDate;
+        this.movie_name = movie_name;
+        this.purchase = purchase;
+    }
 }

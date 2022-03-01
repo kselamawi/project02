6eac5bfbe020af270c4b3a78193d95e4ff412c06
germygrimes <germygrimes@gmail.com>
2022-03-01 14:33:42 -0500

    added JavaMail EmailService, and sendEmailConfirmation function in Purchase.java

--- MTSBoot/src/main/java/com/revature/services/EmailService.java
+++ MTSBoot/src/main/java/com/revature/services/EmailService.java
@@ -1,4 +1,4 @@
-/*
+package com.revature.services;
 import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.*;
@@ -18,6 +18,37 @@ public class EmailService {
         }
         System.exit(0);
     }
-}
 
- */
+    /**
+    * "send" method to send the message.
+    */
+    public static void send(String smtpServer, String to, String from, String subject, String body){
+        try{
+            Properties props = System.getProperties();
+            // -- Attaching to default Session, or we could start a new one --
+            props.put("mail.smtp.host", smtpServer);
+            Session session = Session.getDefaultInstance(props, null);
+            // -- Create a new message --
+            Message msg = new MimeMessage(session);
+            // -- Set the FROM and TO fields --
+            msg.setFrom(new InternetAddress(from));
+            msg.setRecipients(Message.RecipientType.TO,
+            InternetAddress.parse(to, false));
+            // -- We could include CC recipients too --
+            // if (cc != null)
+            // msg.setRecipients(Message.RecipientType.CC
+            // ,InternetAddress.parse(cc, false));
+            // -- Set the subject and body text --
+            msg.setSubject(subject);
+            msg.setText(body);
+            // -- Set some other header information --
+            msg.setHeader("X-Mailer", "LOTONtechEmail");
+            msg.setSentDate(new Date());
+            // -- Send the message --
+            Transport.send(msg);
+            System.out.println("Message sent OK.");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}

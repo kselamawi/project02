90350c17e518182d60477c88f1d80f4e5e36bf84
NabiKalokoh <nabiatu1@gmail.edu>
2022-03-01 17:45:48 -0500

    added visible navbar plus other misc updates

--- movieshowings/src/app/components/logout/logout.component.ts
+++ movieshowings/src/app/components/logout/logout.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
+
+  onSubmit(): void {
+
+    //set cookies...or the session to null?
+    //document.cookie = 'id=;';
+    //document.cookie = 'Authorization=;';
+  }
+
   constructor() { }
 
   ngOnInit(): void {
